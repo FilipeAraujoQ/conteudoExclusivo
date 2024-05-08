8cb71acbb938c10ef7fd5e2cbbaa241b0b744904
Filipe Araujo Queiroz <146143459+FilipeAraujoQ@users.noreply.github.com>
2024-05-07 23:48:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//quando apertar o botão
+var botaoMostrar = document.getElementById('exclusivo-btn');
+const card = document.querySelector('.pop_senha')
+
+// Adiciona um evento de clique ao botão
+botaoMostrar.addEventListener('click', function() {
+    // Adiciona a classe 'show' ao card
+        card.classList.add('show');
+    });
+
+//comprovar a senha
+function logar(){
+    var senha = document.getElementById('password').value;
+
+    if(senha == "admin"){
+        alert('Sucesso');
+        document.querySelector('.pop_senha').style.display = 'none';
+        document.querySelector('#exclusivo-btn').style.display = 'none';
+        document.querySelector('#container_exclusivo').style.filter = 'none';
+    }else{
+        alert('Senha incorreta');
+    }
+
+}
+//pega a o enter
+function verificarEnter(event) {
+    if (event.key === 'Enter') {
+        logar();
+    }
+}
